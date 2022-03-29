deb20546c3246e5a68bc2a3293e80379c5041f38
杨旭彪 <yangxubiao-ls@bestpay.com.cn>
2022-03-29 12:06:26 +0800

    测试 husky

---
+++ hello-world/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:prettier/recommended', // 新增
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    quotes: ['error', 'single'],
+  },
+};
---
+++ hello-world/.prettierrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  semi: true, //句末使用分号
+  singleQuote: true, //使用单引号
+}
