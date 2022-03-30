5e8fe36c1f2e471692792f6cdfe48a4243a814d3
杨旭彪 <yangxubiao-ls@bestpay.com.cn>
2022-03-30 12:46:34 +0800

    feat: 111

---
+++ commitlint.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional', 'cz'],
+  rules: {
+    // Header
+    'header-max-length': [2, 'always', 200],
+    // <type>枚举
+    'type-enum': [
+      2,
+      'always',
+      [
+        'add', // 添加依赖
+        'delete', // 删除代码/文件
+        'feat', // 增加新功能
+        'fix', // 修复bug
+        'style', // 样式修改不影响逻辑
+        'merge', // 合并分支
+        'perfect', // 功能完善
+        'docs', // 修改文档
+        'refactor', // 代码重构
+        'test', // 单元测试修改
+        'ci', // 持续继承
+        'release', // 发布
+        'deploy', // 部署
+        'chore', // 更改配置文件
+        'revert', // 版本回退
+      ],
+    ],
+    // <type> 不能为空
+    'type-empty': [2, 'never'],
+    // <type> 格式 小写
+    'type-case': [2, 'always', 'lower-case'],
+    // <subject> 不能为空
+    'subject-empty': [2, 'never'],
+    // <subject> 以.为结束标志
+    'subject-full-stop': [2, 'never', '.'],
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+const a ='11'
 export default {
   name: 'App',
   components: {
